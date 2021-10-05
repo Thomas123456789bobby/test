f0264d6aacc56f61eb0bfe20f63232560773a42d
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-10-05 11:25:28 +0200

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Annie+Use+Your+Telescope&display=swap" rel="stylesheet">
+    <title>festival</title>
+</head>
+<body>
+<div class="container">
+    
+    <ul class="navbar">
+        <li>home</li>
+        <li>programma</li>
+        <li>tickets</li>
+        
+    </ul>
+    <h1 id="hoofd">Noorderfestival</h1> 
+</div>
+   
+ <section id="section1">
+<img  src="img/lil.jpg" width="500px" height="300px" >
+    <p class="card"> De eerste editie van het Noorderfeest Live gaat in januari van start. Het wordt een unieke ervaring. Een winterfestival met een indrukwekkende line-up. 
+     Bekende artiesten uit binnen- en buitenland treden gedurende twee dagen op. Je kan kaartjes boeken voor het hele weekend of voor 1 dag. Zie het programma* 
+     voor de line-up. Dit is je kans om alsnog een festival te bezoeken dit jaar. Mis deze kans niet en bestel snel kaartjes**!</p>
+</section>
+<section id="section2">
+    <img  src="img/tenten-op-het-woods-mountain-grassy-landscape-camping-site-een-muziekfestival-geel-en-rood-tent-open-grasveld-upstate-new-york-161603195.jpg" height="25%" width="20%"
+    >
+<p class="cardleft">Naast het festivalterrein hebben we een kamperterrein voor bezoekers die twee dagen blijven. Je brengt je eigen tent mee. Als je een kaartje voor twee dagen boekt dan
+     zijn de kosten voor het kamperen inbegrepen. Op het kampeerterrein gelden de volgende regels:
+     
+        ● Geen hard drugs toegestaan! 
+        ● Vanaf 3 uur ‘s nachts moet het stil zijn 
+        ● Geen auto’s op het terrein 
+        ● Laat het terrein netjes achter alsjeblieft</p>
+</section>
+<footer>
+    
+<img id="img" src="img/telefoon.jpg" width="50px" height="50px" > 
+<p>06 28857241</p>  
+<img id="email" src="img/R.png" width="50px" height="50px" >
+<p>165077@student.hor</p>
+</footer>
+    <title>Document</title>
+</head>
+<body>
+ <div class="header"> 
+     <h1> Festival </h1>
+ </div>
+
+<div class="topnav">
+    
+
+<a href="index.php">Home </a>
+<a href="programma.php">programma </a>
+<a href="php/tixets.php">tickets </a>
+
+</div>
+
+
+
+
+
+</body>
+</html>
